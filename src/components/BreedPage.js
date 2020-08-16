4d0ee18d395a11a68f94cab55d8c4edce91f2d70
Shay Buzz <shaybuzz@gmail.com>
2020-08-17 00:35:22 +0300

    dog view for breed

--- src/components/BreedPage.js
+++ src/components/BreedPage.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from "react-dom";
+import Axios from 'axios';
+
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useParams
+    BrowserRouter as Router,
+    useParams
 } from "react-router-dom";
+import DogView from './DogView';
 
-function BreedPage(props){
+function BreedPage(props) {
+    //url params
     let { breed } = useParams();
-    
-    useEffect(()=>{
-        //loading every change
-        console.log(`load page ${breed}`)
-    })
+    const [dogs, setDogs] = useState([])
+    const cardCount = 4
+    const breedUrl = `https://dog.ceo/api/breed/${breed}/images/random/${cardCount}`
 
- 
+    useEffect(() => {
+        //loading every change url
+        console.log('#####load')
+        Axios.get(breedUrl)
+            .then(res => {
+                let theDogs = res.data.message.map(url => <DogView image={url} />)
+                setDogs(theDogs)
+            });
+    }, [breedUrl])
 
 
     return <div>
         the chosen breed :{breed}
+        <p>{dogs}</p>
     </div>
 
 }
